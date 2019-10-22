19f0517c9a3454e85a012ef8d91f97f618f1cac4
sajithmb <56579537+sajithmb@users.noreply.github.com>
2019-10-22 09:56:07 +0530

    asdasd
    asdasd

---
+++ App.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Login from './scen/Login';
+import Signup from './scen/Signup';
+import Home from './scen/Home';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <AppContainer/>
+    );
+  }
+}
+const MainNavigator = createStackNavigator({
+  LoginScreen : Login,
+  SignupScreen : Signup,
+  HomeScreen : Home,
+},
+{initialRouteName : 'LoginScreen'},
+);
+const AppContainer = createAppContainer(MainNavigator);
